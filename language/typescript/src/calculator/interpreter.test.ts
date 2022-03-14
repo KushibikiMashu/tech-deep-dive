88a29b1bb67b61d50a02c780c6e742f1e3076a7c
KushibikiMashu <kushibiki3413@gmail.com>
2022-03-15 05:45:46 +0900

    feat: interpreter のテストケースを追加

--- language/typescript/src/calculator/interpreter.test.ts
+++ language/typescript/src/calculator/interpreter.test.ts
@@ -4,15 +4,90 @@ import Ast from "./ast";
 const interpreter = new Interpreter()
 
 describe('Interpreter', () => {
-  test('add(10, 20)を評価すると、30を返す', () => {
-    const expression = Ast.add(Ast.integer(10), Ast.integer(20))
-    const actual = interpreter.interpret(expression)
-    expect(actual).toBe(30)
+  describe('add', () => {
+    test('add(10, 20)を評価すると、30を返す', () => {
+      const expression = Ast.add(Ast.integer(10), Ast.integer(20))
+      const actual = interpreter.interpret(expression)
+      expect(actual).toBe(30)
+    })
+
+    test('add(10, 0)を評価すると、10を返す', () => {
+      const expression = Ast.add(Ast.integer(10), Ast.integer(0))
+      const actual = interpreter.interpret(expression)
+      expect(actual).toBe(10)
+    })
+
+    test('add(0, 10)を評価すると、10を返す', () => {
+      const expression = Ast.add(Ast.integer(0), Ast.integer(10))
+      const actual = interpreter.interpret(expression)
+      expect(actual).toBe(10)
+    })
   })
 
-  test('add(10, 0)を評価すると、10を返す', () => {
-    const expression = Ast.add(Ast.integer(10), Ast.integer(0))
-    const actual = interpreter.interpret(expression)
-    expect(actual).toBe(10)
+  describe('subtract', () => {
+    test('subtract(10, 20)を評価すると、-10を返す', () => {
+      const expression = Ast.subtract(Ast.integer(10), Ast.integer(20))
+      const actual = interpreter.interpret(expression)
+      expect(actual).toBe(-10)
+    })
+
+    test('subtract(10, 0)を評価すると、10を返す', () => {
+      const expression = Ast.subtract(Ast.integer(10), Ast.integer(0))
+      const actual = interpreter.interpret(expression)
+      expect(actual).toBe(10)
+    })
+
+    test('subtract(0, 10)を評価すると、-10を返す', () => {
+      const expression = Ast.subtract(Ast.integer(0), Ast.integer(10))
+      const actual = interpreter.interpret(expression)
+      expect(actual).toBe(-10)
+    })
+  })
+
+  describe('multiply', () => {
+    test('multiply(10, 20)を評価すると、200を返す', () => {
+      const expression = Ast.multiply(Ast.integer(10), Ast.integer(20))
+      const actual = interpreter.interpret(expression)
+      expect(actual).toBe(200)
+    })
+
+    test('multiply(10, 0)を評価すると、0を返す', () => {
+      const expression = Ast.multiply(Ast.integer(10), Ast.integer(0))
+      const actual = interpreter.interpret(expression)
+      expect(actual).toBe(0)
+    })
+
+    test('multiply(0, 10)を評価すると、0を返す', () => {
+      const expression = Ast.multiply(Ast.integer(0), Ast.integer(10))
+      const actual = interpreter.interpret(expression)
+      expect(actual).toBe(0)
+    })
+  })
+
+  describe('divide', () => {
+    test('divide(10, 20)を評価すると、0.5を返す', () => {
+      const expression = Ast.divide(Ast.integer(10), Ast.integer(20))
+      const actual = interpreter.interpret(expression)
+      expect(actual).toBe(0.5)
+    })
+
+    test('divide(20, 10)を評価すると、2を返す', () => {
+      const expression = Ast.divide(Ast.integer(20), Ast.integer(10))
+      const actual = interpreter.interpret(expression)
+      expect(actual).toBe(2)
+    })
+
+    test('divide(0, 10)を評価すると、0を返す', () => {
+      const expression = Ast.divide(Ast.integer(0), Ast.integer(10))
+      const actual = interpreter.interpret(expression)
+      expect(actual).toBe(0)
+    })
+  })
+
+  describe('exception', () => {
+    test('divide(10, 0)を評価すると、エラーを投げる', () => {
+      const expression = Ast.divide(Ast.integer(10), Ast.integer(0))
+      expect(() => interpreter.interpret(expression)).toThrow()
+    })
   })
 })
--- language/typescript/src/calculator/interpreter.ts
+++ language/typescript/src/calculator/interpreter.ts
@@ -6,6 +6,7 @@ export default class Interpreter {
     if (expression instanceof BinaryExpression) {
       const lhs = this.interpret(expression.lhs)
       const rhs = this.interpret(expression.rhs)
+
       switch (expression.operator) {
         case Operator.ADD:
           return lhs + rhs
@@ -14,6 +15,10 @@ export default class Interpreter {
         case Operator.MULTIPLY:
           return lhs * rhs
         case Operator.DIVIDE:
+          if (rhs === 0) {
+            throw new Error("Division by zero")
+          }
+
           return lhs / rhs
         default:
           throw new Error(`Unknown operator ${expression.operator}`)
